14e42a585937a172e1e1de0b0666d2b8bb603301
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2010-07-12 12:13:46 +0200

    Introduced a new

---
+++ systeminfo.c
@@ -0,0 +1,41 @@
+/*
+** system_info.c
+** 
+** Made by (bengreve)
+** Email   <Benjamin.Negrevergne[at]imag[dot]fr>
+** 
+** Started on  Mon Jul 12 11:22:19 2010 bengreve
+*/
+
+#include <pthread.h>
+#include "systeminfo.h"
+
+
+
+typedef struct{
+  pthread_mutex_t mutex; 
+  pthread_t tids[SYSTEMINFO_MAX_THREADS]; 
+  int next_tid; 
+}thread_ids_t;
+
+thread_ids_t tids; 
+
+int m_register_thread(){
+  pthread_mutex_lock(&tids.mutex); 
+  tids[tids.next_tid++] = pthread_self(); 
+  pthread_mutex_unlock(&tids.mutex);  
+}
+
+int m_thread_id(){
+  pthread_t me =  pthread_self(); 
+  int last = tid.next_tid; 
+  for(int i = 0; i < last; i++){
+    if(me == tids.tids[i])
+      return i; 
+  }
+  assert(false); // if reached, means that the thread has not been registred. 
+}
+
+int m_core_id(){
+  assert(false); //not implemented yet. 
+}
---
+++ systeminfo.h
@@ -0,0 +1,28 @@
+/*
+** systeminfo.h
+** 
+** Made by bengreve
+** Login   <bengreve@confiance.imag.fr>
+** 
+** Started on  Mon Jul 12 11:17:39 2010 bengreve
+** Last update Mon Jul 12 11:17:39 2010 bengreve
+*/
+
+#ifndef   	SYSTEMINFO_H_
+#define   	SYSTEMINFO_H_
+
+
+/** 
+ * \brief attribute a unique id between [0..].
+ * Returned ids can be assumed to be consecutives and starting from 0. 
+ * @return a unique id 
+ */
+int m_register_thread(); 
+int m_thread_id(); 
+
+
+
+int m_core_id(); 
+
+
+#endif 	    /* !SYSTEMINFO_H_ */
