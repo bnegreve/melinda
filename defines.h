928d16b6a265c74221c7f048b751c3038650aa39
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2010-07-12 12:13:46 +0200

    Introduced a systeminfo module, added doxygen config file, create a global definition file.

---
+++ defines.h
@@ -0,0 +1,19 @@
+/*
+** defines.h
+** 
+** Made by bengreve
+** Login   <bengreve@confiance.imag.fr>
+** 
+** Started on  Mon Jul 12 11:25:01 2010 bengreve
+** Last update Mon Jul 12 11:25:01 2010 bengreve
+*/
+
+#ifndef   	DEFINES_H_
+#define   	DEFINES_H_
+#include <assert.h>
+
+
+#define SYSTEMINFO_MAX_THREADS 128
+
+
+#endif 	    /* !DEFINES_H_ */
