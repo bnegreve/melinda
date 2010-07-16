78d39a4e3def0b2d92093a5ba41ceda0fda1b093
Benjamin Negrevergne <Benjamin.Negrevergne@imag.fr>
2010-07-16 09:25:11 +0200

    Added thread.h/.c module that has been forgotten

---
+++ thread.c
@@ -0,0 +1,49 @@
+/*
+** thread.c
+** 
+** Made by (bengreve)
+** Email   <Benjamin.Negrevergne[at]imag[dot]fr>
+** 
+** Started on  Tue Jul 13 16:13:19 2010 bengreve
+*/
+
+#include <pthread.h>
+#include "thread.h"
+#include "defines.h"
+
+typedef struct{
+  pthread_mutex_t mutex; 
+  pthread_t tids[THREAD_MAXTHREADS]; 
+  int next_tid; 
+}thread_ids_t;
+
+thread_ids_t tids; 
+
+int m_thread_register(){
+  pthread_mutex_lock(&tids.mutex); 
+  int tmp = tids.next_tid; 
+  tids.tids[tids.next_tid++] = pthread_self(); 
+  assert(tids.next_tid <= THREAD_MAXTHREADS); 
+  pthread_mutex_unlock(&tids.mutex);  
+  return tmp; 
+}
+
+int m_thread_id(){
+  pthread_t me =  pthread_self(); 
+  int last = tids.next_tid; 
+  for(int i = 0; i < last; i++){
+    if(me == tids.tids[i])
+      return i; 
+  }
+  return THREAD_UNREGISTRED; 
+}
+
+int m_thread_nb_registred(){
+  int tmp; 
+  //TODO locking is quite useless here, a memory barrier should be enough 
+  pthread_mutex_lock(&tids.mutex); 
+  tmp = tids.next_tid; 
+  pthread_mutex_unlock(&tids.mutex); 
+  return tmp; 
+}
+
---
+++ thread.h
@@ -0,0 +1,39 @@
+/*
+** thread.h
+** 
+** Made by bengreve
+** Login   <bengreve@confiance.imag.fr>
+** 
+** Started on  Tue Jul 13 16:10:36 2010 bengreve
+** Last update Tue Jul 13 16:10:36 2010 bengreve
+*/
+
+#ifndef   	THREAD_H_
+# define   	THREAD_H_
+
+
+/** 
+ * \brief attribute a unique id between [0..].
+ * Returned ids can be assumed to be consecutives and starting from 0. 
+ * @return a unique id 
+ */
+int m_thread_register(); 
+
+/** 
+ * \brief Returns a unique id associated to the thread during a former
+ * call to \m_register_thread();
+ *\warning The thread must be registred. 
+ * 
+ * 
+ * @return the unique thread id. 
+ */
+int m_thread_id(); 
+
+/** 
+ * \brief Returns the number of thread.
+ */
+int m_thread_nb_registred(); 
+
+
+
+#endif 	    /* !THREAD_H_ */
